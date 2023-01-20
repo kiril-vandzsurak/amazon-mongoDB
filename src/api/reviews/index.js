d9f3efe7c09a17e89aff40936a5041268b938ad1
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-01-20 14:43:45 +0100

    Completing CRUD for reviews

--- src/api/reviews/index.js
+++ src/api/reviews/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import ReviewModel from "../reviews/model.js";
 import q2m from "query-to-mongo";
-
+import createHttpError from "http-errors";
 const reviewRouter = express.Router();
 
 reviewRouter.post("/", async (req, res, next) => {
@@ -32,6 +32,57 @@ reviewRouter.get("/", async (req, res, next) => {
       totalPages: Math.ceil(total / mongoQuery.options.limit),
       reviews,
     });
+  } catch (error) {
+    next(error);
+  }
+});
+
+reviewRouter.get("/:reviewId", async (req, res, next) => {
+  try {
+    const review = await ReviewModel.findById(req.params.reviewId);
+    if (review) {
+      res.send(review);
+    } else {
+      next(
+        createHttpError(404, `Review with id ${req.params.reviewId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+reviewRouter.put("/:reviewId", async (req, res, next) => {
+  try {
+    const updatedReview = await ReviewModel.findByIdAndUpdate(
+      req.params.reviewId,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (updatedReview) {
+      res.send(updatedReview);
+    } else {
+      next(
+        createHttpError(404, `Review with id ${req.params.reviewId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+reviewRouter.delete("/:reviewId", async (req, res, next) => {
+  try {
+    const deletedReview = await ReviewModel.findByIdAndDelete(
+      req.params.reviewId
+    );
+    if (deletedReview) {
+      res.status(204).send();
+    } else {
+      next(
+        createHttpError(404, `Review with id ${req.params.reviewId} not found!`)
+      );
+    }
   } catch (error) {
     next(error);
   }
