832926ddab259801614ee27fae658e616015333f
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-01-20 10:49:57 +0100

    Creating endpoint GET for reviews

--- src/api/reviews/index.js
+++ src/api/reviews/index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import createHttpError from "http-errors";
 import ReviewModel from "../reviews/model.js";
+import q2m from "query-to-mongo";
 
 const reviewRouter = express.Router();
 
@@ -9,6 +9,29 @@ reviewRouter.post("/", async (req, res, next) => {
     const newReview = await ReviewModel(req.body);
     const { _id } = await newReview.save();
     res.status(201).send({ _id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+reviewRouter.get("/", async (req, res, next) => {
+  try {
+    const mongoQuery = q2m(req.query);
+    const total = await ReviewModel.countDocuments(mongoQuery.criteria);
+
+    const reviews = await ReviewModel.find(
+      mongoQuery.criteria,
+      mongoQuery.options.fields
+    )
+      .limit(mongoQuery.options.limit)
+      .skip(mongoQuery.options.skip)
+      .sort(mongoQuery.options.sort);
+
+    res.send({
+      links: mongoQuery.links("http://localhost:3001/reviews", total),
+      totalPages: Math.ceil(total / mongoQuery.options.limit),
+      reviews,
+    });
   } catch (error) {
     next(error);
   }
